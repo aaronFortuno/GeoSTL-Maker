147d82778aae0fb3ccfc375c51179796b657b6f1
Aarón Fortuño <101064283+aaronFortuno@users.noreply.github.com>
2026-02-05 16:54:32 +0100

    new files

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("No s'ha trobat l'element arrel per muntar l'aplicació");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Aquest fitxer està buit per evitar errors 404. 
+// Tota la lògica s'ha mogut a index.html per compatibilitat directa amb GitHub Pages.
